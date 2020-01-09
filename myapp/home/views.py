957091f2570338acebcadbc1f6440e4a1e68ef52
lee-jeong-geun <ljk0411jg@naver.com>
2020-01-10 00:05:23 +0900

    test

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from myapp.item.models import Item
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
+
 def test(request):
-    item_list = Item.objects.all()
     return render(request, 'test.html', {})
