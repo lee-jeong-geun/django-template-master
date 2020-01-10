e76ba56c3a28eea9119349ed6150b1675e68d65c
lee-jeong-geun <ljk0411jg@naver.com>
2020-01-11 00:54:48 +0900

    test

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -11,6 +11,6 @@ def index(request):
 
 
 def test(request):
-    item = Item.objects.filter(Id=False).order_by('Id')
+    item = Item.objects.all().order_by('Id')
     item_list = serializers.serialize('json', item)
     return HttpResponse(item_list)
