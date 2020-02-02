9b5d6cfaac40afb77d6d25d1669791b93be6e687
lee-jeong-geun <ljk0411jg@naver.com>
2020-02-03 02:32:00 +0900

    create skin_type score function

--- myapp/item/models.py
+++ myapp/item/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+skin_type_map = {'oily': 0, 'dry': 0, 'sensitive': 0}
+score_map = {'O': 1, 'X': -1, '': 0}
 
 
 class Ingredient(models.Model):
@@ -12,21 +14,19 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
-    def __str__(self, type):
-        if type == "oily":
-            return self.oily
-        elif type == "dry":
-            return self.dry
-        return self.sensitive
+    def calculate_score(self, skin_type):
+        skin_type_map['oily'] = score_map[str(self.oily)]
+        skin_type_map['dry'] = score_map[str(self.dry)]
+        skin_type_map['sensitive'] = score_map[str(self.sensitive)]
+        return skin_type_map[skin_type]
 
 
 class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     imageId = models.CharField(max_length=40)
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=80)
     price = models.IntegerField()
     gender = models.CharField(max_length=6)
     category = models.CharField(max_length=10)
     ingredients = models.ManyToManyField(Ingredient, related_name='product_ingredient')
     monthlySales = models.IntegerField()
-
